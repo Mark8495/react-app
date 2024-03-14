9338da62b49b8fdf835337560f7f87f47b2513cd
Mark Byrne <markbyrne8495@gmail.com>
2024-03-14 10:47:12 +0000

    added es7+

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New York", "Tokyo", "London", "Paris", "San Francisco"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities"></ListGroup>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
